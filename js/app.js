0f25393c0cbac9d756f69a41666e90d643174cd6
KnottBound <danielgknott@gmail.com>
2023-11-03 18:06:04 -0400

    Create Website Start
    - Added in Logo
    - Basic Yellow/Black CSS
    - Basic JavaScript Overview
    - Basic Events, Event Listener, Event Handler
    - Three JS parts.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//The Document is an Object. The Write is a Method write().
+document.write("<h2>This was written in JavaScript<h2>");
+
+function makeYellow() {
+    // This is a simple function named 'makeYellow'.
+    
+    // 'document' is the global object representing the HTML document in the browser.
+    // 'getElementById' is a method of 'document' that returns a reference to the element by its ID.
+    // 'style' is a property of the HTML element that allows us to change its inline CSS styles.
+    // 'color' is a property of the 'style' object representing the text color of the element.
+    // 'yellow' is the value we're assigning to the 'color' property, changing the text color to yellow.
+    document.getElementById('event').style.color ='yellow';
+}
+
+function makeGreen () {
+    document.getElementById('event').style.color ='green';
+}
+
+// Creating Get JS Button 'js-button' from DOM, Add Event Listener with Event Listener 'Click' and Event Handler Method jsBtnClick() ,
+document.getElementById('js-button').addEventListener('click', jsBtnClick = () => {
+    document.getElementById('event').style.color ='orange';
+})
